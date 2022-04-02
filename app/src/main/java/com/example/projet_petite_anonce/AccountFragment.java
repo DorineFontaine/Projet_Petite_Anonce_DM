9a26a1ec1a698827fbd55d273da79e0ad4c252dd
DorineFontaine <dorinefont@gmail.com>
2022-04-02 15:47:01 +0400

    Debut du design des fragments

--- app/src/main/java/com/example/projet_petite_anonce/AccountFragment.java
+++ app/src/main/java/com/example/projet_petite_anonce/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.projet_petite_anonce;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class AccountFragment extends Fragment {
 
+    TextView inscription;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view =  inflater.inflate(R.layout.fragment_account, container, false);
+        inscription = view.findViewById(R.id.inscription);
+
+        //Intent page_inscription = new Intent(this, InscriptionActivity.class);
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // startActivity(page_inscription);
+
+            }
+        });
+        return view;
     }
 }
