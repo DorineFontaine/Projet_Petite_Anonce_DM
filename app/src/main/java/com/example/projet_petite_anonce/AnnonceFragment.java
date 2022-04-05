0f2d1a1f4d2eee3936c1e19bf721ea445934384f
DorineFontaine <dorinefont@gmail.com>
2022-04-05 12:14:22 +0400

    design MessageFragment

--- app/src/main/java/com/example/projet_petite_anonce/AnnonceFragment.java
+++ app/src/main/java/com/example/projet_petite_anonce/AnnonceFragment.java
@@ -10,11 +10,13 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 
-public class MessageFragment extends Fragment {
+public class AnnonceFragment extends Fragment {
+
     ListView simpleList;
 
-    String nomAcheteur[] = {"Manon ", " Dorine ",  "Louis  ", "Tristan"};
-    String prix[] = {"Bonjour, je suis... ", "Bonjour, toujour...", "Bonjours, tj disp...", "Toujours dispo?.."};
+
+    String ville[] = {"GARE SAINT ROCH ", " TOULOUSE ",  "ILE DE  FRANCE  ", "PARIS"};
+    String prix[] = {"15$ ", "5$", "90$", "20$"};
     String temps[] = {"Aujourd'hui à 15h ", " 24/05/2022", "24/12/2022", "17/03/2022"};
     String titre[] = {"Robe ", " BD", "Chaussure Nike", "Chaussure à talon"};
     int photo[]={R.drawable.robe,R.drawable.bd,R.drawable.chaussure_nike,R.drawable.chaussure};
@@ -25,10 +27,10 @@ public class MessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View view = inflater.inflate(R.layout.fragment_message, container, false);
-        simpleList = (ListView) view.findViewById(R.id.listview_message);
-        CustomAdaptater customAdapter = new CustomAdaptater(getContext(),titre ,temps, prix, photo,nomAcheteur,  R.layout.list_view_item);
+        View view = inflater.inflate(R.layout.fragment_annonce, container, false);
+        simpleList = (ListView) view.findViewById(R.id.listview);
+        CustomAdaptater customAdapter = new CustomAdaptater(getContext(), ville, prix, temps,photo,titre,  R.layout.list_view_item);
         simpleList.setAdapter(customAdapter);
-       return view;
+        return view;
     }
 }
