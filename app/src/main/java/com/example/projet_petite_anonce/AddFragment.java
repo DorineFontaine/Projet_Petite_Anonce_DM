f69f8240038c16ee52f9097f45583e83bd2a76b3
manette-gif <73953040+manette-gif@users.noreply.github.com>
2022-04-05 13:34:00 +0200

    Update AddFragment.java

--- app/src/main/java/com/example/projet_petite_anonce/AddFragment.java
+++ app/src/main/java/com/example/projet_petite_anonce/AddFragment.java
@@ -7,18 +7,58 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class AddFragment extends Fragment {
 
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
 
+    private int mParam1;
+
+    public AddFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment AddFragment.
+     */
+    public static AddFragment newInstance(int param1) {
+        AddFragment fragment = new AddFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getInt(ARG_PARAM1);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add, container, false);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(view.getContext(), R.layout.dropdown_item, getResources().getStringArray(R.array.feelings));
+        AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.autoCompleteTextView);
+        autoCompleteTextView.setAdapter(arrayAdapter);
 
         return view;
     }
