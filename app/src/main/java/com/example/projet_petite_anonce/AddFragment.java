976af2b96b68dc6162eb288af90359457f3a7023
manette-gif <73953040+manette-gif@users.noreply.github.com>
2022-04-06 15:28:02 +0200

    Update AddFragment.java

--- app/src/main/java/com/example/projet_petite_anonce/AddFragment.java
+++ app/src/main/java/com/example/projet_petite_anonce/AddFragment.java
@@ -6,26 +6,174 @@ import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;package projetAndroid.petitesannonces;
+
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class AddFragment extends Fragment {
 
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
 
+    private int mParam1;
+
+    private View view;
+    private MapView map;
+    private IMapController mapController;
+
+    public AddFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment AddFragment.
+     */
+    public static AddFragment newInstance(int param1) {
+        AddFragment fragment = new AddFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getInt(ARG_PARAM1);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_add, container, false);
+        view = inflater.inflate(R.layout.fragment_add, container, false);
         ArrayAdapter arrayAdapter = new ArrayAdapter(view.getContext(), R.layout.dropdown_item, getResources().getStringArray(R.array.categorie));
         AutoCompleteTextView autoCompleteTextView = view.findViewById(R.id.autoCompleteTextView);
         autoCompleteTextView.setAdapter(arrayAdapter);
 
+        mapCreation();
+
         return view;
+    }
+
+    public void mapCreation(){
+        Configuration.getInstance().load(getActivity().getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext()));
+
+        map = view.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        GeoPoint starPoint =new GeoPoint(43.610769 , 3.876716);//ma map est centrée sur montpellier
+        mapController = map.getController();
+        mapController.setCenter(starPoint);
+        mapController.setZoom(13.0); //entre 0 et 25 nombre flotant FAUT JOUER AVEC LE ZOOM
+
+        //définition des boutons
+        Button btn_localisation = view.findViewById(R.id.btn_localisation);
+        btn_localisation.setOnClickListener(view -> convertAddressToCoord());
+
+    }
+
+    public void convertAddressToCoord(){
+
+        //recuperer ce qui est dans l'edit text
+        EditText editText = view.findViewById(R.id.editTextLocalisation);
+        String localisation = editText.getText().toString();
+
+        //recuperer les coordonnées de l'adresse
+
+        try {
+            Geocoder geocoder = new Geocoder(view.getContext(), Locale.FRENCH);
+            List<Address> adresses = geocoder.getFromLocationName(localisation, 1);
+            Address address = adresses.get(0);
+            Double longitude = address.getLongitude();
+            Double latitude = address.getLatitude();
+
+            ArrayList<OverlayItem> items = new ArrayList<>();
+            OverlayItem item = new OverlayItem("Mon annonce", address.getAddressLine(0), new GeoPoint(latitude, longitude));
+
+            //on change le style du marker
+            Drawable drawable = getActivity().getDrawable(R.drawable.ic_baseline_location_on_24);
+            item.setMarker(drawable);
+            items.add(item);
+            //on recentre la map sur le marker
+            mapController.setCenter(new GeoPoint(latitude, longitude));
+
+
+
+            ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<>(getActivity().getApplicationContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+
+                @Override
+                public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                    //quand je clique dessus
+                    return true;
+                }
+
+                @Override
+                public boolean onItemLongPress(int index, OverlayItem item) {
+                    return false;
+                }
+            });
+
+            //ca focus quand on clique dessus
+            mOverlay.setFocusItemsOnTap(true);
+            map.getOverlays().add(mOverlay);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(view.getContext(), "Veuillez entrer une adresse valide svp", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
     }
 }
